f48668000a51f016969505286a9660b9c16c3dcc
김광용 <veta6287@naver.com>
2022-10-25 17:28:49 +0900

    Revert "Merge branch 'feat/be/issue-83' into 'master'"
    This reverts merge request !1

--- backend/Kafka-Server/src/main/java/com/GeekHub/KafkaServer/constant/KafkaConstants.java
+++
@@ -1,7 +0,0 @@
-package com.GeekHub.KafkaServer.constant;
-
-public class KafkaConstants {
-    public static final String KAFKA_TOPIC = "LocationLogs";
-    public static final String GROUP_ID = "test";
-    public static final String KAFKA_BROKER = "13.125.100.94:9092";
-}
--- backend/Kafka-Server/src/main/java/com/GeekHub/KafkaServer/repository/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.GeekHub.KafkaServer.repository;
-
-import com.GeekHub.KafkaServer.model.Message;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MessageRepository extends MongoRepository<Message, String> {
-
-}
