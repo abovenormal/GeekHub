99c0ed33d8c4ab6a7ed7b254e07cb458efdc8db3
SSAFY <origene42@naver.com>
2022-10-27 17:40:51 +0900

    [feat]chatting&camerapage&배달시작페이지연결

--- GeekHub/app/src/main/java/com/example/geekhub/DeliveryCameraFragment.kt
+++ GeekHub/app/src/main/java/com/example/geekhub/DeliveryCameraFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import com.example.geekhub.databinding.FragmentDeliveryCameraBinding
 
 class DeliveryCameraFragment : Fragment() {
@@ -16,8 +17,14 @@ class DeliveryCameraFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentDeliveryCameraBinding.inflate(inflater,container,false)
+
+        binding.sendPicture.setOnClickListener{
+            (activity as MainActivity).changeFragment(5)
+
+        }
         // Inflate the layout for this fragment
         return binding.root
     }
+
 
 }
--- GeekHub/app/src/main/java/com/example/geekhub/MainActivity.kt
+++ GeekHub/app/src/main/java/com/example/geekhub/MainActivity.kt
@@ -1,19 +1,16 @@
 package com.example.geekhub
 
-import android.Manifest
 import android.content.pm.PackageManager
-import android.location.LocationManager
 import android.os.Bundle
-import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.add
 import androidx.fragment.app.commit
 import com.example.geekhub.databinding.ActivityMainBinding
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationCallback
-import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 
 
@@ -76,6 +73,11 @@ class MainActivity : AppCompatActivity() {
                 moveFragment(DeliveryCameraFragment())
             }
 
+            5 -> {
+                clearBackStack()
+                moveFragment(DeliveryFragment())
+            }
+
         }
 
 
@@ -89,6 +91,12 @@ class MainActivity : AppCompatActivity() {
             .commit()
 
     }
+
+
+fun clearBackStack() {
+    supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+}
+
 
 
 
