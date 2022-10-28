3a9e51eaa2fd0d34da42370d024bd51c14dd0a90
김광용 <veta6287@naver.com>
2022-10-28 11:14:11 +0900

    feat : 길ì경로 찾기 api 구현

--- backend/app/src/main/java/com/deleat/geekhub/MainActivity.kt
+++ backend/app/src/main/java/com/deleat/geekhub/MainActivity.kt
@@ -1,10 +1,11 @@
 package com.deleat.geekhub
 
+
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
-import androidx.fragment.app.FragmentManager
+import androidx.annotation.UiThread
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.PathOverlay
@@ -15,34 +16,47 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback{
-    companion object{
-        lateinit var naverMap: NaverMap
-    }
-
-    private lateinit var mapView: MapView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fm = supportFragmentManager
+
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+        mapFragment.getMapAsync(this)
+    }
+
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+//        val coord = LatLng(37.5670135, 126.9783740)
+//        Toast.makeText(this@MainActivity, "위도: ${coord.latitude}, 경도: ${coord.longitude}", Toast.LENGTH_SHORT).show()
         val APIKEY_ID = "dsd1syl3bg"
         val APIKEY = "WuDvwWaBFLTvSzduM94HkD7wLsrsInigPd4SFHF0"
-        //레트로핏 객체 생성
-        val retrofit =
-            Retrofit.Builder().baseUrl("https://naveropenapi.apigw.ntruss.com/map-direction/")
-                .addConverterFactory(GsonConverterFactory.create()).build()
+
+        val retrofit = Retrofit.Builder().
+        baseUrl("https://naveropenapi.apigw.ntruss.com/map-direction/").
+        addConverterFactory(GsonConverterFactory.create()).
+        build()
+
         val api = retrofit.create(NaverAPI::class.java)
-        //근처에서 길찾기
-        val callgetPath =
-            api.getPath(APIKEY_ID, APIKEY, "129.089441, 35.231100", "129.084454, 35.228982")
-        callgetPath.enqueue(object : Callback<ResultPath> {
+
+        val callgetPath = api.getPath(APIKEY_ID, APIKEY,"129.089441, 35.231100", "129.084454, 35.228982")
+
+        callgetPath.enqueue(object : Callback<ResultPath>{
             override fun onResponse(
                 call: Call<ResultPath>,
                 response: Response<ResultPath>
             ) {
                 var path_cords_list = response.body()?.route?.traoptimal
+
                 val path = PathOverlay()
-                val path_container: MutableList<LatLng>? = mutableListOf(LatLng(0.1, 0.1))
-                for (path_cords in path_cords_list!!) {
-                    for (path_cords_xy in path_cords?.path!!) {
+
+                val path_container : MutableList<LatLng>? = mutableListOf(LatLng(0.1, 0.1))
+                for (path_cords in path_cords_list!!){
+                    for(path_cords_xy in path_cords?.path!!){
                         path_container?.add(LatLng(path_cords_xy[1], path_cords_xy[0]))
                     }
                 }
@@ -51,11 +65,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback{
                 path.map = naverMap
 
                 if (path.coords != null) {
-                    val cameraUpdate = CameraUpdate.scrollTo(path.coords[0]!!)
-                        .animate(CameraAnimation.Fly, 3000)
+                    val cameraUpdate = CameraUpdate.scrollTo(path.coords[0]!!).animate(CameraAnimation.Fly, 3000)
                     naverMap!!.moveCamera(cameraUpdate)
 
-                    Toast.makeText(this@MainActivity, "경로안내내", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this@MainActivity, "경로안내가 시작됩니다.", Toast.LENGTH_SHORT).show()
+
 
                 }
             }
@@ -66,7 +80,5 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback{
         })
     }
 
-    override fun onMapReady(p0: NaverMap) {
-        TODO("Not yet implemented")
-    }
+
 }
