46001109025c1763eb953acbe7f1c3e0f38c236e
positivehun <jihun3104@gmail.com>
2022-11-02 11:05:48 +0900

    feat:Eureka Client

--- backend/Kafka-Server/src/main/java/com/GeekHub/KafkaServer/KafkaServerApplication.java
+++ backend/Kafka-Server/src/main/java/com/GeekHub/KafkaServer/KafkaServerApplication.java
@@ -2,8 +2,10 @@ package com.GeekHub.KafkaServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class KafkaServerApplication {
 
 	public static void main(String[] args) {
