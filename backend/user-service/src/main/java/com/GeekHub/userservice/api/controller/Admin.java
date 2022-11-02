5b36b3f34bfbbed1b5e5b25373008895c520a1e3
한세환 <abovenormal5023@gmail.com>
2022-11-02 15:17:46 +0900

    refactor: cors 수정

--- backend/user-service/src/main/java/com/GeekHub/userservice/api/controller/Admin.java
+++ backend/user-service/src/main/java/com/GeekHub/userservice/api/controller/Admin.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/admin")
-@CrossOrigin(origins = {"http:localhost:3000", "http:k7c205.p.ssafy.io:3000"}, allowCredentials = "true")
+@CrossOrigin(origins = {"http://localhost:3000", "http://k7c205.p.ssafy.io:3000"}, allowCredentials = "true")
 public class Admin {
 
     private final AdminService adminService;
