c7b3bbc9ecadbe7ad7a93c11fb4ca31cae4253b4
정재철 <paneko3@gmail.com>
2022-11-07 14:34:38 +0900

    feat: eureka 설정 eureka 설정 추가

--- backend/Task-Server/src/main/java/com/GeekHub/TaskServer/TaskServerApplication.java
+++ backend/Task-Server/src/main/java/com/GeekHub/TaskServer/TaskServerApplication.java
@@ -2,8 +2,10 @@ package com.GeekHub.TaskServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TaskServerApplication {
 
 	public static void main(String[] args) {
