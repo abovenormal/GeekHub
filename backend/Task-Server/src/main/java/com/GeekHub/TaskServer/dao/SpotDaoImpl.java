4e4f76899298b35b5a34519e7529114d36ade847
김광용 <veta6287@naver.com>
2022-11-07 10:02:27 +0900

    fix : 작업 정보 API 수정

--- backend/Task-Server/src/main/java/com/GeekHub/TaskServer/dao/SpotDaoImpl.java
+++ backend/Task-Server/src/main/java/com/GeekHub/TaskServer/dao/SpotDaoImpl.java
@@ -72,6 +72,7 @@ public class SpotDaoImpl implements SpotDao {
                     int minute = spot.getExpected_time().getMinute();
                     time = hour + "시" + minute + "분";
                     WorkResponseDto workResponseDto = new WorkResponseDto();
+                    workResponseDto.setSpotIndex(String.valueOf(spot.getSpotIdx()));
                     workResponseDto.setSpotName(spot.getSpotName());
                     workResponseDto.setExpected_time(time);
                     workResponseDto.setCount(spot.getCount());
