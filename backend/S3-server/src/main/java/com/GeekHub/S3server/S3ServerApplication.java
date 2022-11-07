729cbc1179187a33d631031e86bfccb9a79a337f
한세환 <abovenormal5023@gmail.com>
2022-11-07 14:37:49 +0900

    refactor: eureka 서버 활성화 (포헌님 요청)

--- backend/S3-server/src/main/java/com/GeekHub/S3server/S3ServerApplication.java
+++ backend/S3-server/src/main/java/com/GeekHub/S3server/S3ServerApplication.java
@@ -2,8 +2,10 @@ package com.GeekHub.S3server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class S3ServerApplication {
 
 	public static void main(String[] args) {
