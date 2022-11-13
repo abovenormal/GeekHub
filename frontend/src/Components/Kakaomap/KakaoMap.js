0dbc00bb3383867e5924184ceb21b6ad03f37a95
Jaecheol Jeong <paneko3@gmail.com>
2022-11-13 23:17:43 +0900

    feat: 근무중인 배달기사 조회 페이지 구현

--- frontend/src/Components/Kakaomap/KakaoMap.js
+++ frontend/src/Components/Kakaomap/KakaoMap.js
@@ -1,56 +1,72 @@
 /* global kakao */
-import { usePickerState } from "@mui/x-date-pickers/internals/hooks/usePickerState";
 import React, { useEffect, useState } from "react";
 import { apiInstance } from "../../api/index";
 import Dropdown from "../Common/Dropdown";
 import "./css/KakaoMap.css";
 import { Map, MapMarker, CustomOverlayMap } from "react-kakao-maps-sdk";
+import cityJson from "./city.json";
+import schoolJson from "./school.json";
+import markerImg from "../../asset/image/marker.png";
 
 function KakaoMap() {
+  const [data, setData] = useState([]);
   const [state, setState] = useState({
     center: { lat: 35.19919101818564, lng: 126.87300478078876 },
     isPanto: false,
     level: 7,
   });
+
   const [selected, setSelected] = useState({
     localCity: "",
     localSchool: "",
   });
+  const [preSchool, setPreSchool] = useState("");
+
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await apiInstance().get("spot/workingDriver");
+        console.log(res.data);
+        setData(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+    getData();
+  }, []);
 
   useEffect(() => {
     console.log(selected);
-    if (selected.localCity == "광주") {
-      setState((prev) => {
-        return {
-          ...prev,
-          center: { lat: 35.19919101818564, lng: 126.87300478078876 },
-          level: 7,
-        };
-      });
-    } else if (selected.localCity == "서울") {
-      setState((prev) => {
-        return {
-          ...prev,
-          center: { lat: 37.540876286520124, lng: 126.98383067483235 },
-          level: 9,
-        };
-      });
-    } else if (selected.localCity == "인천") {
-      setState((prev) => {
-        return {
-          ...prev,
-          center: { lat: 37.45516317286201, lng: 126.70391641355518 },
-          level: 9,
-        };
-      });
-    } else if (selected.localCity == "수원") {
-      setState((prev) => {
-        return {
-          ...prev,
-          center: { lat: 37.2614848228648, lng: 127.03042637316096 },
-          level: 9,
-        };
-      });
+    console.log(state)
+    console.log(preSchool)
+    for (let i = 0; i < cityJson.length; i++) {
+      if (cityJson[i].localCity == selected.localCity) {
+        setState((prev) => {
+          return {
+            ...prev,
+            center: { lat: cityJson[i].center.lat, lng: cityJson[i].center.lng },
+            level: cityJson[i].level,
+          };
+        });
+        if (preSchool == selected.localSchool) {
+          break;
+        }
+        for (let j = 0; j < schoolJson.length; j++) {
+          if (schoolJson[j].localSchool == selected.localSchool) {
+            console.log(selected.localSchool);
+            setState((prev) => {
+              return {
+                ...prev,
+                center: { lat: schoolJson[j].center.lat, lng: schoolJson[j].center.lng },
+                level: schoolJson[j].level,
+              };
+            });
+            setPreSchool(selected.localSchool);
+            break;
+          }
+        }
+        break;
+      }
     }
   }, [selected]);
 
@@ -65,11 +81,39 @@ function KakaoMap() {
         level={state.level}
         style={{ width: "100%", height: "80vh" }}
       >
-        <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-          <div style={{ color: "#000" }}>Hello World!</div>
-        </MapMarker>
+        {data.map((driver, index) => (
+          <>
+            <CustomOverlayMap // 커스텀 오버레이를 표시할 Container
+              // 커스텀 오버레이가 표시될 위치입니다
+              position={{
+                lat: driver.lat,
+                lng: driver.lon,
+              }}
+            >
+              {/* 커스텀 오버레이에 표시할 내용입니다 */}
+              <div className="label" style={{ color: "#0e1737" }}>
+                <span className="label-span">{driver.userName}</span>
+              </div>
+            </CustomOverlayMap>
+            <MapMarker
+              key={`${driver.userName}`}
+              position={{
+                lat: driver.lat,
+                lng: driver.lon,
+              }} // 마커를 표시할 위치
+              image={{
+                src: markerImg, size: {
+                  width: 24,
+                  height: 35
+                },
+              }}
+              title={driver.userName} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
+            >
+            </MapMarker>
+          </>
+        ))}
       </Map>
-    </div>
+    </div >
   );
 }
 export default KakaoMap;
