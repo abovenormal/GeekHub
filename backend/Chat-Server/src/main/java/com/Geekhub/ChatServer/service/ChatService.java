2de60919ecac52f98ba94967ca52f4453117c597
김광용 <veta6287@naver.com>
2022-11-16 10:29:26 +0900

    feat : getMessage API 작성

--- backend/Chat-Server/src/main/java/com/Geekhub/ChatServer/service/ChatService.java
+++ backend/Chat-Server/src/main/java/com/Geekhub/ChatServer/service/ChatService.java
@@ -1,11 +1,15 @@
 package com.Geekhub.ChatServer.service;
 
+import com.Geekhub.ChatServer.client.AdminClient;
+import com.Geekhub.ChatServer.dto.MessageDto;
+import com.Geekhub.ChatServer.dto.UserInfoDto;
 import com.Geekhub.ChatServer.model.Message;
 import com.Geekhub.ChatServer.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Slf4j
@@ -13,18 +17,23 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ChatService {
     private final MessageRepository messageRepository;
+    private final AdminClient adminClient;
 
-    public List<Message> FindPeopleByRoomId(String roomId){
+    public List<MessageDto> FindPeopleByRoomId(String roomId){
 
-        log.info("service hi");
-        List<Message> members = messageRepository.findMessageByRoomId(roomId).orElse(null);
-        System.out.println(roomId);
-        System.out.println(members.size());
+        List<MessageDto> result = new ArrayList<>();
+        List<Message> members = messageRepository.findMessagesByRoomIdOrderByTimestamp(roomId).orElse(null);
         for(Message s: members){
-            System.out.println(s);
+            UserInfoDto userInfoDto = adminClient.userInfo(s.getSender());
+            MessageDto messageDto = new MessageDto();
+            messageDto.setCreated_at(s.getTimestamp());
+            messageDto.setUserId(s.getSender());
+            messageDto.setContent(s.getContent());
+            messageDto.setName(userInfoDto.getName());
+            result.add(messageDto);
         }
 
-        return members;
+        return result;
     }
 
 
