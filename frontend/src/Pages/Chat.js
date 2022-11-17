e50d28494ab9fc553c3fa7e377cb77041ab8f6d4
김광용 <veta6287@naver.com>
2022-11-17 17:52:08 +0900

    feat : chat

--- frontend/src/Pages/Chat.js
+++ frontend/src/Pages/Chat.js
@@ -10,7 +10,11 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import axios from "axios";
+import SockJS from "sockjs-client";
+import { over } from 'stompjs';
+import { now } from "moment";
 
+let stompClient = null;
 const Chat = () => {
   const scrollRef = useRef();
 
@@ -20,6 +24,53 @@ const Chat = () => {
   const [chat, setChat] = useState([]);
   const [chatMap, setChatMap] = useState([]);
   const [roomName, setRoomName] = useState("");
+  const [roomIdx, setRoomIdx] = useState("");
+  const [message, setMessage] = useState("");
+  const sendChatHandler = async () => {
+    if (message === '') return;
+    const now = new Date();
+    const utcNow = now.getTime() + (now.getTimezoneOffset() * 60 * 1000);
+    const koreaTimeDiff = 9 * 60 * 60 * 1000;
+    const koreaNow = new Date(utcNow + koreaTimeDiff);
+    const time = new Date()
+    time.setHours(koreaNow.getHours)
+    time.setMinutes(koreaNow.getMinutes)
+    console.log(time)
+    const chatMessage = {
+      sender : "1",
+      content : message,
+      roomId : roomIdx,
+      timestamp : new Date()
+    };
+    console.log(chatMessage)
+    console.log(JSON.stringify(chatMessage))
+    stompClient.send(`/app/sendMessage`, {}, JSON.stringify(chatMessage));
+    setMessage('');
+
+  }
+
+  const onError = err => {
+    throw err;
+  }
+
+  const onMessageReceived = () => {
+    console.log("메시지 받았니?")
+    axios("http://k7c205.p.ssafy.io:8088/chat/message", {
+              method: "GET",
+              params: {
+                roomIdx: roomIdx,
+              },
+            })
+              .then((res) => {
+                console.log(res);
+                setChat(res.data);
+              })
+              .catch((err) => console.log("Update Price error", err));
+  }
+
+  const onConnected = () => {
+    stompClient.subscribe(`/chat/${roomIdx}`, onMessageReceived);
+  }
 
   async function getData() {
     try {
@@ -33,6 +84,13 @@ const Chat = () => {
   function createData(name, calories, fat, carbs, protein) {
     return { name, calories, fat, carbs, protein };
   }
+  useEffect(() => {
+    let Sock = new SockJS("http://k7c205.p.ssafy.io:8088/endpoint")
+    stompClient = over(Sock)
+    stompClient.debug = null;
+    console.log(roomIdx)
+    stompClient.connect({}, onConnected, onError);
+  }, [roomIdx])
   useEffect(() => {
     getData();
   }, []);
@@ -57,6 +115,8 @@ const Chat = () => {
         <div
           className="discussion message-active"
           onClick={(e) => {
+            // console.log(rows[i].id)
+            setRoomIdx(rows[i].id)
             setRoomName(rows[i].localSchool);
             axios("http://k7c205.p.ssafy.io:8088/chat/message", {
               method: "GET",
@@ -65,12 +125,13 @@ const Chat = () => {
               },
             })
               .then((res) => {
-                console.log(res);
+                // console.log(res);
                 setChat(res.data);
               })
               .catch((err) => console.log("Update Price error", err));
             console.log(rows[i]);
           }}
+
         >
           <div className="desc-contact">
             <p className="name">{rows[i].localSchool}</p>
@@ -135,11 +196,14 @@ const Chat = () => {
                 type="text"
                 className="write-message"
                 placeholder="메세지 입력"
+                value={message}
+                onChange={e => setMessage(e.target.value)}
               ></input>
               <i
                 className="icon send fa fa-paper-plane-o clickable"
                 aria-hidden="true"
                 c0js0
+                onClick={sendChatHandler}
               ></i>
             </div>
           </div>
